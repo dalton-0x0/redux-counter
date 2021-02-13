d2b7d3a9e903eba37b1a4f7994b6cf1d50555a99
Dalton Okechukwu <okechukwu@whitewhale.ai>
2021-02-12 17:23:23 -0700

    installed redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import './index.css';
 import App from './App';
@@ -25,7 +26,12 @@ const logger = (store) => {
     };
 };
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
     <React.StrictMode>
